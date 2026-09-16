5983d1ed563ddf2ba141ecaab31070a264b30315
kkkyl666-sys <kkkyl666@gmail.com>
2026-09-16 20:46:30 +0800

    Publish private dusk study pet shell

---
+++ sync-config.js
@@ -0,0 +1,5 @@
+// Fill these two values after creating the personal Supabase project.
+window.PET_SYNC_CONFIG = {
+  supabaseUrl: "https://cehltciekjflygkhezvp.supabase.co",
+  supabaseAnonKey: "sb_publishable_V2lCSWdnDedvFrgSHLGN_w_sWdhfPcz"
+};
