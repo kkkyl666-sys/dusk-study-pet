46d6fe21083d7a70db70e622bc49e1a14a9328ba
kkkyl666-sys <kkkyl666@gmail.com>
2026-09-16 23:36:33 +0800

    Replace email login with device access code

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dusk-study-pet-v2";
+const CACHE_NAME = "dusk-study-pet-v3";
 const APP_SHELL = ["./", "./index.html", "./dusk-pet.png", "./manifest.webmanifest", "./sync-config.js"];
 
 self.addEventListener("install", event => {
