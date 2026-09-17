dfe595869312932f45fcc4578210ca25df8d11ee
kkkyl666-sys <kkkyl666@gmail.com>
2026-09-17 22:53:05 +0800

    Show British phonetics for vocabulary

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dusk-study-pet-v7";
+const CACHE_NAME = "dusk-study-pet-v8";
 const APP_SHELL = ["./", "./index.html", "./dusk-pet.png", "./manifest.webmanifest", "./sync-config.js", "./cet6-35.js"];
 
 self.addEventListener("install", event => {
