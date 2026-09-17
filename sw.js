3018f5d029d35bbeeb88c075996abaad1501b5a8
kkkyl666-sys <kkkyl666@gmail.com>
2026-09-17 23:09:54 +0800

    Use local IPA dictionary for vocabulary

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dusk-study-pet-v9";
+const CACHE_NAME = "dusk-study-pet-v10";
 const APP_SHELL = ["./", "./index.html", "./dusk-pet.png", "./manifest.webmanifest", "./sync-config.js", "./cet6-35.js"];
 
 self.addEventListener("install", event => {
