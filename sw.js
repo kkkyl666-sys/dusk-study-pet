19070ee9d83aad4190104927629454ce13e6f5d0
kkkyl666-sys <kkkyl666@gmail.com>
2026-09-21 23:32:00 +0800

    Improve mobile pronunciation playback

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dusk-study-pet-v11";
+const CACHE_NAME = "dusk-study-pet-v12";
 const APP_SHELL = ["./", "./index.html", "./dusk-pet.png", "./manifest.webmanifest", "./sync-config.js", "./cet6-35.js"];
 
 self.addEventListener("install", event => {
