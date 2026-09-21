448d3f2549bd61ef978346faf17133314aea1357
kkkyl666-sys <kkkyl666@gmail.com>
2026-09-21 23:43:56 +0800

    Add curated examples for CET6 words

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dusk-study-pet-v12";
+const CACHE_NAME = "dusk-study-pet-v13";
 const APP_SHELL = ["./", "./index.html", "./dusk-pet.png", "./manifest.webmanifest", "./sync-config.js", "./cet6-35.js"];
 
 self.addEventListener("install", event => {
