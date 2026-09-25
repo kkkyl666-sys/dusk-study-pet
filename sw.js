452b3d9e7d444409fa3ce4ab388046cb3bf9a790
kkkyl666-sys <kkkyl666@gmail.com>
2026-09-26 00:53:12 +0800

    Use 4am English study-day boundary

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dusk-study-pet-v15";
+const CACHE_NAME = "dusk-study-pet-v16";
 const APP_SHELL = ["./", "./index.html", "./dusk-pet.png", "./manifest.webmanifest", "./sync-config.js", "./cet6-35.js"];
 
 self.addEventListener("install", event => {
